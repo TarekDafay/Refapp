e4f191ed41f87e757a5b329b61e13d424c3da777
Tarek Dafay <Tarek.Dafay@gmx.de>
2024-09-27 11:58:43 +0200

    [REFAPP-03] Add Forward and Backward seeking functionality

--- lib/Playbar.dart
+++ lib/Playbar.dart
@@ -14,17 +14,42 @@ class Playbar extends StatefulWidget {
 
 class PlaybarState extends State<Playbar> {
   bool isPlaying = false;
+
   void playVideo() {widget.controller.play();}
   void pauseVideo() {widget.controller.pause();}
+  void seekForward(int sec) { 
+    Duration nextPos = widget.controller.value.position - Duration(seconds: sec);
+    if(nextPos <= widget.controller.value.duration)  {
+      widget.controller.seekTo(widget.controller.value.position + Duration(seconds: sec));
+    } else  {
+      widget.controller.seekTo(widget.controller.value.duration);
+    }  
+  }
+
+  void seekBackward(int sec) {
+    Duration nextPos = widget.controller.value.position - Duration(seconds: sec);
+    if(nextPos >= const Duration(seconds: 0))  {
+      widget.controller.seekTo(widget.controller.value.position - Duration(seconds: sec));
+    } else  {
+      widget.controller.seekTo(const Duration(seconds: 0));
+    }
+  }
 
   @override
   Widget build(BuildContext context)  {
     var buttons = [
         ElevatedButton(
-          onPressed: () {},
-          child: const Icon(Icons.fast_rewind)
+          onPressed: () { setState(() {
+            seekBackward(30);
+          });},
+          child: const Icon(Icons.replay_30)
         ),
-        
+        ElevatedButton(
+          onPressed: () { setState(() {
+            seekBackward(10);
+          });},
+          child: const Icon(Icons.replay_10)
+        ),
         ElevatedButton(
         onPressed: () {setState(() { 
         widget.controller.value.isPlaying
@@ -36,8 +61,16 @@ class PlaybarState extends State<Playbar> {
             child: Icon(!widget.controller.value.isPlaying ? Icons.play_arrow : Icons.pause),
         ),
         ElevatedButton(
-          onPressed: () { },
-          child: const Icon(Icons.fast_forward)
+          onPressed: () { setState(() {
+            seekForward(10);
+          }); },
+          child: const Icon(Icons.forward_10)
+        ),
+        ElevatedButton(
+          onPressed: () { setState(() {
+            seekForward(30);
+          }); },
+          child: const Icon(Icons.forward_30)
         ),
       ];
     if(MediaQuery.orientationOf(context) == Orientation.portrait)  {
