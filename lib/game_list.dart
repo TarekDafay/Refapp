4900065bfc090709aaf9009d810f88bba5f69295
Makussu <maximilian@ruhm.info>
2024-12-01 22:06:01 +0100

    formatted date and sorted goals

--- lib/game_list.dart
+++ lib/game_list.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 
 // Api documentation available at https://api.openligadb.de/index.html
@@ -27,6 +28,11 @@ class _MatchesListViewState extends State<MatchesListView> {
     } else {
       throw Exception('Failed to load matches');
     }
+  }
+
+  String formatDate(String dateTime) {
+    final DateTime parsedDate = DateTime.parse(dateTime);
+    return DateFormat('dd.MM.yyyy').format(parsedDate);
   }
 
   @override
@@ -59,12 +65,13 @@ class _MatchesListViewState extends State<MatchesListView> {
             itemCount: matchesData.length,
             itemBuilder: (context, index) {
               final match = matchesData[index];
+              final String formattedDate = formatDate(match['matchDateTime']);
               return GestureDetector(
                 onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => MatchDetailScreen(match: match),
+                      builder: (context) => MatchDetailScreen(match: match, formattedDate: formattedDate),
                     ),
                   );
                 },
@@ -88,30 +95,45 @@ class _MatchesListViewState extends State<MatchesListView> {
 class MatchDetailScreen extends StatelessWidget {
   final Map<String, dynamic> match;
 
-  const MatchDetailScreen({Key? key, required this.match}) : super(key: key);
+  final String formattedDate;
+
+  const MatchDetailScreen({Key? key, required this.match, required this.formattedDate}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
+    final sortedGoals = List<Map<String, dynamic>>.from(match['goals'] ?? []);
+    sortedGoals.sort((a, b) => a['matchMinute'].compareTo(b['matchMinute']));
     return Scaffold(
       appBar: AppBar(
-        title: Text('${match['team1']['teamName']} vs ${match['team2']['teamName']}'),
+        title: Text('${match['team1']['shortName']} vs ${match['team2']['shortName']}'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Match ID: ${match['matchID']}'),
-            Text('League: ${match['leagueName']}'),
-            Text('Date: ${match['matchDateTime']}'),
-            Text('Location: ${match['location']['locationCity']} - ${match['location']['locationStadium']}'),
+            Text('${match['group']['groupName']}'),
+            Text('Liga: ${match['leagueName']}'),
+            Text('Datum: $formattedDate'),
+            Text('Wo: ${match['location']['locationCity']} - ${match['location']['locationStadium']}'),
             const SizedBox(height: 16),
             Text('Team 1: ${match['team1']['teamName']}'),
             Text('Team 2: ${match['team2']['teamName']}'),
             const SizedBox(height: 16),
-            if (match['goals'] != null && match['goals'].isNotEmpty)
-              ...match['goals'].map<Widget>((goal) => Text(
-                  'Minute ${goal['matchMinute']}: ${goal['goalGetterName']} (${goal['scoreTeam1']} - ${goal['scoreTeam2']})')),
+            Text("Goals:", style: const TextStyle(fontWeight: FontWeight.bold)),
+              if (sortedGoals.isNotEmpty)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Goals:', style: TextStyle(fontWeight: FontWeight.bold)),
+                  ...sortedGoals.map((goal) => Text(
+                      'Minute ${goal['matchMinute']}: ${goal['goalGetterName']} (${goal['scoreTeam1']} - ${goal['scoreTeam2']})')),
+                ],
+              )
+            else
+              const Text('No goals in this match'),
           ],
         ),
       ),
