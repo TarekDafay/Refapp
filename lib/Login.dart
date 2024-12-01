b8753c3eba234f3496f2d9a9203858f0d4d55314
Tarek Dafay <tarek@pop-os.speedport.ip>
2024-12-01 23:54:46 +0100

    [REFAPP_05] Finalize Login Page

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ref_app/FormContainerWidget.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -43,10 +45,45 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold))
-        ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+              SizedBox(height: 30),
+              FormContainerWidget(
+                hintText: "Email",
+                isPasswordField: false
+              ),
+              SizedBox(height: 10),
+              FormContainerWidget(
+                  hintText: "Password",
+                  isPasswordField: true
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style:
+                      TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                ),
+              )
+            ],
+          ),
+      ),
       ),
     );
   }
