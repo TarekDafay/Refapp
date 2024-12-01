0baae956e2c3983c8695bc5e4c97719cbe53c2d2
Tarek Dafay <tarek@pop-os.speedport.ip>
2024-12-01 23:28:26 +0100

    [REFAPP_05] Setup LoginPage

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
-  _LoginState createState() => _LoginState();
+  State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
   @override
+  void initState()  {
+    Future.delayed(
+      Duration(seconds: 3), () {
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
+    }
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(
+            "Welcome to the REFAPP",
+            style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold
+            )
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
   }
 }
