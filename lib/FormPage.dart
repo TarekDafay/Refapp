8ef07b1aa5872cfc7a689efb3cf1daebfae36614
Tarek Dafay <tarek@pop-os.speedport.ip>
2024-12-02 02:57:29 +0100

    [REFAPP_05] Add Signup and Signin functionality

--- lib/FormPage.dart
+++ lib/FormPage.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ref_app/FormContainerWidget.dart';
+import 'package:ref_app/firebase_auth_services.dart';
 import 'package:ref_app/home_screen.dart';
 import 'package:ref_app/toolbar.dart';
 
@@ -8,25 +10,48 @@ enum FormType {
   SIGNUP,
 }
 
-class FormPage extends StatelessWidget {
+class FormPage extends StatefulWidget {
   final FormType? formType;
   const FormPage({super.key, this.formType});
 
+  @override
+  State<StatefulWidget> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+
+  final FireBaseAuthService _auth = FireBaseAuthService();
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   String getFormName()  {
-    switch(formType!) {
+    switch(widget.formType!) {
       case FormType.LOGIN:
         return "Login";
       case FormType.SIGNUP:
         return "Signup";
     }
+  }
 
+  bool isLogIn() {
+    return widget.formType == FormType.LOGIN;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text(getFormName()),
       ),
       body: Center(
         child: Padding(
@@ -37,25 +62,34 @@ class FormPage extends StatelessWidget {
               Text(getFormName(), style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
               SizedBox(height: 30),
               Visibility(
-                visible: formType == FormType.SIGNUP,
+                visible: !isLogIn(),
                 child: FormContainerWidget(
                   lowerPadding: 10,
                   hintText: "Username",
                   isPasswordField: false,
+                  controller: _userNameController,
                 ),
               ),
               FormContainerWidget(
                   lowerPadding: 10,
                   hintText: "Email",
-                  isPasswordField: false
+                  isPasswordField: false,
+                  controller: _emailController,
               ),
               FormContainerWidget(
                   lowerPadding: 30,
                   hintText: "Password",
-                  isPasswordField: true
+                  isPasswordField: true,
+                  controller: _passwordController,
               ),
               GestureDetector(
-                onTap: () { Navigator.pushNamed(context, "/home"); },
+                onTap: () {
+                  if(isLogIn()) {
+                    signIn();
+                  } else  {
+                    signUp();
+                  }
+                },
                 child: Container(
                 width: double.infinity,
                 height: 45,
@@ -75,31 +109,66 @@ class FormPage extends StatelessWidget {
                 ),
               ),
               ),
-              Visibility(
-                visible: formType == FormType.LOGIN,
-                child:
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("You don't have an account?"),
-                      SizedBox(width: 5),
-                      GestureDetector(
-                        onTap: () { Navigator.pushNamed(context, "/signup"); },
-                        child: Text(
-                          "Sign Up!",
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      )
 
-                    ],
-                  ),
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(isLogIn() ? "You don't have an account?" : "You already have an account?"),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      if(isLogIn()) {
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, "/signup", (Route<
+                            dynamic> route) => false);
+                      } else  {
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, "/login", (Route<
+                            dynamic> route) => false);
+                      }
+                    },
+                    child: Text(
+                      isLogIn() ? "Sign Up" : "Login",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
         ],
         ),
       ),
     ));
+  }
+
+  void signUp() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signUpWithEmailAndPassword(email, password);
+
+    if(user!= null) {
+      print("User successfully created");
+      Navigator.pushNamedAndRemoveUntil(context, "/home",(Route<dynamic> route) => false);
+    } else  {
+      print("Some error happened");
+    }
+  }
+
+  void signIn() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signInWithEmailAndPassword(email, password);
+
+    if(user!= null) {
+      Navigator.pushNamedAndRemoveUntil(context, "/home",(Route<dynamic> route) => false);
+      print("User successfully logged In");
+    } else  {
+      print("Some error happened");
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ref_app/Login.dart';
 import 'package:ref_app/Signup.dart';
 import 'package:ref_app/toolbar.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
